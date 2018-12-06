447968ae232cd6ccaef6176b23ed0e30fb6e6d2b
Richard Mauri <15126243+richard-mauri@users.noreply.github.com>
2018-12-06 14:34:07 -0800

    Seprste the demo build step from the demo run step -helps travis build

--- demo.sh
+++ demo.sh
@@ -11,82 +11,13 @@
 # It then starts an ssh server which is configured to trust the vault ca.
 # Next, it builds and launches the vaultssh client that starts an interactive cert based ssh session.
 
-if [ "${GOPATH}" = "" ]; then
-    echo "Please set the GOPATH environment variable; can't build vaultssh"
-    exit 1
-fi
-
-export VAULT_ADDR=http://localhost:8200
-export VAULT_TOKEN=roottoken
-
-pkill vault >& /dev/null
-docker kill sshtest >& /dev/null
-docker rm sshtest >& /dev/null
-
-vault server -dev -dev-root-token-id=roottoken > vaultinit.out &
-
-sleep 1
-
-# The following steps should be done by an admin user 
-vault secrets enable -version=1 kv
-vault secrets enable -path=ssh ssh
-vault auth enable userpass
-
-vault policy write ssh-ubuntu-user - <<"EOH1"
-path "ssh/sign/regular-role" {
-    capabilities = ["create","update"]
-}
-path "ssh/sign/regular-role" {
-    capabilities = ["create","update"]
-}
-path "kv/users/ubuntu/*" {
-    capabilities = ["create", "read", "update", "delete", "list"]
-}
-path "kv/data/users/ubuntu/*" {
-    capabilities = ["create", "read", "update", "delete", "list"]
-}
-EOH1
-
-vault write auth/userpass/users/ubuntu \
-    password=newpasswd \
-    policies=ssh-ubuntu-user
-
-vault write -field=public_key ssh/config/ca generate_signing_key=true
-
-vault read -field=public_key ssh/config/ca > ./trusted-user-ca-keys.pem
-
-docker build --tag sshtest .
-
-docker run -p 6061:22 -d --name sshtest sshtest
-
-vault write ssh/roles/regular-role -<<"EOH"
-{
-  "allow_user_certificates": true,
-  "allowed_users": "*",
-  "default_extensions": [
-    {
-      "permit-pty": ""
-    }
-  ],
-  "key_type": "ca",
-  "default_user": "ubuntu",
-  "ttl": "30m0s"
-}
-EOH
-
-BUILDTIME=$(date +%m-%d-%Y-%H:%M)
-
-go install -ldflags "-X github.com/richard-mauri/vaultssh/vs.VersionString=v0.1.0-${BUILDTIME}"
-
-go test github.com/richard-mauri/vaultssh/vs
-
-# In a real scenario the user wishing to ssh would perform addkey once and the ssh often (not back to back like here)
+. ./demo-build.sh
 
 # Typically, vaultssh -mode addkey is run on users pc and there is network access to the vault server
+
 $GOPATH/bin/vaultssh -mode addkey -publicKeyPath ~/.ssh/id_rsa.pub -privateKeyPath ~/.ssh/id_rsa -username ubuntu -passwd newpasswd
 
 # Typically, vaultssh -mode ssh is run on bastion host which also has network access to the vault server
 # (Note: the system will prompt for the vault userpass credential if not provided)
 
-# Commenting out the following line so the travis ci build will not hang on the interactive ssh session - this will be sorted out
-#$GOPATH/bin/vaultssh -mode ssh -sshServerPort 6061 -username ubuntu -passwd newpasswd
+$GOPATH/bin/vaultssh -mode ssh -sshServerPort 6061 -username ubuntu -passwd newpasswd
